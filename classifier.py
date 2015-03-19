e9888a4449df7327af15a93f28745bbcdee84df0
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-19 10:58:58 -0400

    Count messages as model is trained.

--- classifier.py
+++ classifier.py
@@ -6,14 +6,18 @@ class Model(object):
     def __init__(self):
         self.__spam = defaultdict(int)
         self.__ham = defaultdict(int)
+        self.__n_spam = 0
+        self.__n_ham = 0
 
     def get_words(self, text):
         return text.split()
 
     def train_spam(self, text):
+        self.__n_spam += 1
         for word in self.get_words(text):
             self.__spam[word] += 1
 
     def train_ham(self, text):
+        self.__n_ham += 1
         for word in self.get_words(text):
             self.__ham[word] += 1
