14bcfc8675864f72ea0b5e8ef10a0099879375eb
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-21 14:35:17 -0400

    Use top 15 most interesting words.

--- classifier.py
+++ classifier.py
@@ -49,11 +49,15 @@ class Model(object):
         spamicity = spam_frequency / (ham_frequency + spam_frequency)
         return max(self.min_spamicity, min(self.max_spamicity, spamicity))
 
+    def most_interesting(self, spamicities, n=15):
+        return sorted(spamicities, key=lambda x: abs(x-0.5), reverse=True)[:n]
+
     def classify_text(self, text):
         if self.__n_ham == 0 or self.__n_spam == 0:
             raise NotTrained()
 
         spamicities = map(self.classify_word, self.get_words(text))
+        spamicities = self.most_interesting(spamicities)
         hamicities = map(lambda x: 1-x, spamicities)
         spam_frequency = max(self.min_spamicity, exp(sum(log(s) for s in spamicities)))
         ham_frequency = max(self.min_spamicity, exp(sum(log(s) for s in hamicities)))
