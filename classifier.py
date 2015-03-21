c52ff7082708ca6e3ea88782bb62f2151b5c9416
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-03-21 11:12:18 -0400

    Classify text.

--- classifier.py
+++ classifier.py
@@ -1,6 +1,9 @@
 from __future__ import division
 import re
 from collections import defaultdict
+from math import log, exp
+
+class NotTrained(Exception): pass
 
 class Model(object):
     """A model trained on spam and ham, used for classifying other text."""
@@ -31,6 +34,9 @@ class Model(object):
             self.__ham[word.lower()] += 1
 
     def classify_word(self, word):
+        if self.__n_ham == 0 or self.__n_spam == 0:
+            raise NotTrained()
+
         spam_count = self.__spam[word.lower()]
         ham_count = 2 * self.__ham[word.lower()]
 
@@ -41,3 +47,15 @@ class Model(object):
         spam_frequency = min(self.max_spamicity, spam_count / self.__n_spam)
         spamicity = spam_frequency / (ham_frequency + spam_frequency)
         return max(self.min_spamicity, min(self.max_spamicity, spamicity))
+
+    def classify_text(self, text):
+        if self.__n_ham == 0 or self.__n_spam == 0:
+            raise NotTrained()
+
+        spamicities = map(self.classify_word, self.get_words(text))
+        hamicities = map(lambda x: 1-x, spamicities)
+        spam_frequency = exp(sum(log(s) for s in spamicities))
+        ham_frequency = exp(sum(log(s) for s in hamicities))
+        spamicity = spam_frequency / (ham_frequency + spam_frequency)
+        return max(self.min_spamicity, min(self.max_spamicity, spamicity))
+
